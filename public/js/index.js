e9c6c1289f3c9aae23c048da145a5bf29d8f6d1c
Rodney Huynh <rhuynh2005@gmail.com>
2019-02-19 09:41:50 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,10 @@ var socket = io();
 
 socket.on('connect', function ()  {
   console.log('Connected to server')
-
-   socket.emit('createMessage', {
-    from: 'client@gmail.com',
-    text: 'justs sent a message from client'
-  })
 })
 
 socket.on('newMessage', function (message) {
-  console.log('Got new message: ', message)
+  console.log('newMessage: ', message)
 })
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected')
 
-  socket.emit('newMessage', {
-    from: 'johndoe@mail.com',
-    text: 'Hey, can you meet up at 6?',
-    createdAt: 123
-  })
-  
   socket.on('createMessage', (message) => {
     console.log('Received message from client:', message)
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
 
 
